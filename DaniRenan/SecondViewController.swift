20baa97a8b760a26bacbae0c7de62c73530243dd
renanbrando <reebrando@gmail.com>
2017-10-05 21:18:58 -0300

    Added initial files.

---
+++ DaniRenan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DaniRenan
+//
+//  Created by Renan Ribeiro Brando on 05/10/17.
+//  Copyright © 2017 Renan Ribeiro Brando. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
