09c7a0716ba5e15070657895f447f06c90c16e63
renanbrando <reebrando@gmail.com>
2017-10-13 23:37:15 -0300

    Implemented states crud, where the user can add, edit or remove an state in the settings screen.

--- DaniRenan/BagViewController.swift
+++ DaniRenan/BagViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  ProductsTableViewController.swift
 //  DaniRenan
 //
 //  Created by Renan Ribeiro Brando on 05/10/17.
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import CoreData
 
-class BagViewController: UIViewController {
+class BagViewController: UITableViewController {
+    
+    var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 22))
+    var fetchedResultController: NSFetchedResultsController<Product>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 106
+        tableView.rowHeight = UITableViewAutomaticDimension
+        label.text = "Sua lista está vazia!"
+        label.textAlignment = .center
+        label.textColor = .white
+        
+        loadMovies()
+    }
+    
+    
+    func loadMovies() {
+        let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+        do {
+            try fetchedResultController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Table view data source
+    
+    //Método que define a quantidade de seções de uma tableView
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //Método que define a quantidade de células para cada seção de uma tableView
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = fetchedResultController.fetchedObjects?.count {
+            tableView.backgroundView = (count == 0) ? label : nil
+            return count
+        } else {
+            tableView.backgroundView = label
+            return 0
+        }
+    }
 
+}
 
+// MARK: - NSFetchedResultsControllerDelegate
+extension BagViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
+    }
 }
 
--- DaniRenan/SettingsViewController.swift
+++ DaniRenan/SettingsViewController.swift
@@ -7,20 +7,30 @@
 //
 
 import UIKit
+import CoreData
+
+enum CategoryType {
+    case add
+    case edit
+}
 
 class SettingsViewController: UIViewController {
 
     // MARK: - IBOutlets
     @IBOutlet weak var tfExchange: UITextField!
     @IBOutlet weak var tfIOF: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
-    var dataSource: [Category] = []
+    var dataSource: [State] = []
     var product: Product!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadStates()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,7 +54,94 @@ class SettingsViewController: UIViewController {
         
         UserDefaults.standard.set(tfIOF.text!, forKey: "iof")
     }
+    
+    // MARK: - Methods
+    func loadStates() {
+        let fetchRequest: NSFetchRequest<State> = State.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        do {
+            dataSource = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func showAlert(type: CategoryType, state: State?) {
+        let title = (type == .add) ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: "\(title) Estado", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Nome da estado"
+            if let name = state?.name {
+                textField.text = name
+            }
+        }
+        alert.addAction(UIAlertAction(title: title, style: .default, handler: { (action: UIAlertAction) in
+            let state = state ?? State(context: self.context)
+            state.name = alert.textFields?.first?.text
+            do {
+                try self.context.save()
+                self.loadStates()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func add(_ sender: Any) {
+        showAlert(type: .add, state: nil)
+    }
+   
 
+
+}
+
+// MARK: - UITableViewDelegate
+extension SettingsViewController: UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Excluir") { (action: UITableViewRowAction, indexPath: IndexPath) in
+            let state = self.dataSource[indexPath.row]
+            self.context.delete(state)
+            try! self.context.save()
+            self.dataSource.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "Editar") { (action: UITableViewRowAction, indexPath: IndexPath) in
+            let state = self.dataSource[indexPath.row]
+            tableView.setEditing(false, animated: true)
+            self.showAlert(type: .edit, state: state)
+        }
+        editAction.backgroundColor = .blue
+        return [editAction, deleteAction]
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SettingsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let state = dataSource[indexPath.row]
+        cell.textLabel?.text = state.name
+        cell.accessoryType = .none
+        if product != nil {
+            //if let states = product.states, states.contains(state) {
+              //  cell.accessoryType = .checkmark
+            //}
+        }
+        return cell
+    }
 
 }
 
